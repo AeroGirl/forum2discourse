22604b37f092c5d29752aaf0b8d556e481988779
Channing <ChanningDai@gmail.com>
2014-02-16 23:06:46 +0800

    Update user model

--- lib/forum2discourse/models/vanilla/user.rb
+++ lib/forum2discourse/models/vanilla/user.rb
@@ -6,14 +6,11 @@ class Forum2Discourse::Models::Vanilla::User
   property :id, Serial, field: "UserID"
   property :username, String, field: "Name"
   property :email,      String, field: "Email"
-  property :firstname,  String, field: "FirstName"
-  property :lastname,   String, field: "LastName"
 
   def to_discourse
     Forum2Discourse::Models::Discourse::User.new(
       username: username,
-      email: email,
-      name: "#{firstname} #{lastname}"
+      email: email
     )
   end
 end

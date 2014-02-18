1615878138d0eb8785ddb01db89301814e05d071
Channing <ChanningDai@gmail.com>
2014-02-18 12:31:13 +0800

    Handle discussion body as a comment

--- lib/forum2discourse/models/vanilla/discussion.rb
+++ lib/forum2discourse/models/vanilla/discussion.rb
@@ -7,17 +7,28 @@ class Forum2Discourse::Models::Vanilla::Discussion
   property :created_at,   DateTime, field: 'DateInserted'
   property :category_id,  Integer, field: 'CategoryID'
   property :subject,      Text, field: 'Name'
+  property :body, Text, field: 'Body'
+  property :user_id, Integer, field: 'InsertUserID'
 
   has n, :comments, 'Forum2Discourse::Models::Vanilla::Comment'
   belongs_to :category, 'Forum2Discourse::Models::Vanilla::Category'
 
   def to_discourse
     return nil if category.nil?
+
+    topic_comment = Forum2Discourse::Models::Vanilla::Comment.new(
+      created_at: created_at,
+      discussion_id: id,
+      user_id: user_id,
+      body: body)
+
+    all_comments = comments.unshift(topic_comment)
+
     Forum2Discourse::Models::Discourse::Topic.new({
       title: subject,
       created_at: created_at,
       category: category.name,
-      posts: comments.map(&:to_discourse)
+      posts: all_comments.map(&:to_discourse)
     })
   end
 end
